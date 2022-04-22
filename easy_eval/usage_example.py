dbd52db80f0abf02ddc7d0282c4d7854a0f54eef
Xi <xiye17@gmail.com>
2022-04-22 17:51:33 -0500

    include easy to use tools for eval

---
+++ easy_eval/usage_example.py
@@ -0,0 +1,14 @@
+from eval import check_equiv, check_io_consistency
+
+# check equivalance
+print('EXPECTED TRUE', check_equiv('or(<low>,<cap>)', 'or(<cap>,<low>)'))
+print('EXPECTED TRUE',check_equiv('or(<low>,<cap>)', '<let>'))
+print('EXPECTED FALSE',check_equiv('cat(<low>,<cap>)', 'cat(<cap>,<low>)'))
+
+# check example consistency
+
+spec = 'and(repeatatleast(or(<low>,or(<cap>,<^>)),1),and(not(startwith(<low>)),and(not(startwith(<^>)),not(contain(concat(notcc(<low>),<^>))))))'
+good_examples = [('Vg^Peg^', '+'), ('CQzyPeqLgZFMpo^c^', '+'), ('UMoaHHz^', '+'), ('KyugLn^h^', '+'), ('CAhPZtBtvfCcuLtBvnR', '+'), ('MFt^Qh^czg^SMwWg', '+'), (('Vg^Peg^', '+'), '-'), (('CQzyPeqLgZFMpo^c^', '+'), '-'), (('UMoaHHz^', '+'), '-'), (('KyugLn^h^', '+'), '-'), (('CAhPZtBtvfCcuLtBvnR', '+'), '-'), (('MFt^Qh^czg^SMwWg', '+'), '-')]
+bad_examples = [(';PP;p!mC!FB;;;TI', '+'), (';!;V!RDq;;;X', '+'), (';X;;;I', '+'), (';MW!usG!FEPBiTP;;;P', '+'), ('PnPw;UaPQPYPY;;;X', '+'), ('SRP!P!;;;AN', '+'), ((';PP;p!mC!FB;;;TI', '+'), '-'), ((';!;V!RDq;;;X', '+'), '-'), ((';X;;;I', '+'), '-'), ((';MW!usG!FEPBiTP;;;P', '+'), '-'), (('PnPw;UaPQPYPY;;;X', '+'), '-'), (('SRP!P!;;;AN', '+'), '-')]
+print('EXPECTED TRUE',check_io_consistency(spec, good_examples))
+print('EXPECTED FALSE',check_io_consistency(spec, bad_examples))
