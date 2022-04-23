a54e36cfaec98ce9e9679f2f2782f8b3537300e0
Xi <xiye17@gmail.com>
2022-04-23 01:29:39 -0500

    code fragment for converting and parsing

--- easy_eval/usage_example.py
+++ easy_eval/usage_example.py
@@ -1,4 +1,5 @@
 from eval import check_equiv, check_io_consistency
+from streg_utils import parse_spec_to_ast
 
 # check equivalance
 print('EXPECTED TRUE', check_equiv('or(<low>,<cap>)', 'or(<cap>,<low>)'))
@@ -12,3 +13,9 @@ good_examples = [('Vg^Peg^', '+'), ('CQzyPeqLgZFMpo^c^', '+'), ('UMoaHHz^', '+')
 bad_examples = [(';PP;p!mC!FB;;;TI', '+'), (';!;V!RDq;;;X', '+'), (';X;;;I', '+'), (';MW!usG!FEPBiTP;;;P', '+'), ('PnPw;UaPQPYPY;;;X', '+'), ('SRP!P!;;;AN', '+'), ((';PP;p!mC!FB;;;TI', '+'), '-'), ((';!;V!RDq;;;X', '+'), '-'), ((';X;;;I', '+'), '-'), ((';MW!usG!FEPBiTP;;;P', '+'), '-'), (('PnPw;UaPQPYPY;;;X', '+'), '-'), (('SRP!P!;;;AN', '+'), '-')]
 print('EXPECTED TRUE',check_io_consistency(spec, good_examples))
 print('EXPECTED FALSE',check_io_consistency(spec, bad_examples))
+
+
+# skeleton for converting to standard regex
+ast = parse_spec_to_ast(spec)
+print(ast.logical_form())
+std_regex = ast.standard_regex()
